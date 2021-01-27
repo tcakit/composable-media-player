7eccab0328430e089fa25aca1c1d413521ab84ff
Joe Blau <im@joeblau.com>
2021-01-26 16:45:23 -0800

    Copy module logic from https://github.com/joeblau/swift-composable-kit

--- Sources/ComposableMediaPlayer/ComposableMediaPlayer.swift
+++
@@ -1,3 +0,0 @@
-struct ComposableMediaPlayer {
-    var text = "Hello, World!"
-}
--- Tests/ComposableMediaPlayerTests/ComposableMediaPlayerTests.swift
+++ Tests/ComposableMediaPlayerTests/ComposableMediaPlayerTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class ComposableMediaPlayerTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ComposableMediaPlayer().text, "Hello, World!")
+		XCTAssertTrue(true)
     }
 
     static var allTests = [
